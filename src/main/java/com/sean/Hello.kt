301a44e134aaae7352deaa2a64780887d9f42e84
Sean <xinan1019@gmail.com>
2024-12-12 10:31:54 +0800

    variable,data type

--- src/main/java/com/sean/Hello.kt
+++ src/main/java/com/sean/Hello.kt
@@ -5,6 +5,12 @@ fun main() {
 //    Human().Hello();
     var human = Human();
     human.Hello();
+    val age : Int = 19; //val 名稱 : 型態 = 值
+    //val 不可改變 var 可變
+
+    var weight = 66.5f;
+    var name : String;
+    name = "Sean";
 }
 class Human{
     fun Hello(){
