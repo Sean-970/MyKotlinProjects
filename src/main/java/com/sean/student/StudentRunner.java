df80fbd9a6ca8d040ac9ce4f74bbf7bb08724d4c
Sean <xinan1019@gmail.com>
2024-12-12 17:17:54 +0800

    Student.kt finish

--- src/main/java/com/sean/student/StudentRunner.java
+++ src/main/java/com/sean/student/StudentRunner.java
@@ -1,8 +1,18 @@
 package com.sean.student;
 
+import java.util.Scanner;
+
 public class StudentRunner {
     public static void main(String[] args) {
-        Student s1 = new Student("Sean",66,88);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.next();
+        System.out.println("Enter your english:");
+        int english = sc.nextInt();
+        System.out.println("Enter your math:");
+        int math = sc.nextInt();
+        Student s1 = new Student(name,english,math);
         s1.print();
+        sc.close();
     }
 }
--- src/main/kotlin/com/sean/student/Student.kt
+++ src/main/kotlin/com/sean/student/Student.kt
@@ -1,8 +1,15 @@
 package com.sean.kotlin
 
 fun main() {
-    println("test")
-    val s1 = Student("Sean",80,70)
+    val scanner = java.util.Scanner(System.`in`); // 關鍵字 + `
+    println("Enter your name:")
+    var name = scanner.nextLine();
+    println("Hello $name")
+    println("Enter your english score :")
+    var english = scanner.nextInt();
+    println("Enter your math score :")
+    var math = scanner.nextInt();
+    val s1 = Student(name,english,math)
     s1.print()
 }
 class Student (var name : String,var english : Int,var math : Int){
