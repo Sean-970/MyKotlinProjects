ab0c0c99f4f26ff5659a549a4bdfdf437645160e
Sean <xinan1019@gmail.com>
2024-12-12 10:42:16 +0800

    variable,data type

--- src/main/java/com/sean/Hello.java
+++ src/main/java/com/sean/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
         System.out.println("Hello");
         Person person = new Person();
         person.hello();
+        int i = 5;
+        Integer i1 = 3;
+        System.out.println(i1.byteValue());
+        char c = '好';
+        Character c1 = '你';
+        byte b = 127;
+        float weight = 60.5f;
+        long l = 31131243;
+        boolean whether = true;
+        String name = "Sean";
     }
 }
