1bc793362647ed376aac4d2b4be4a077a532ff63
Sean <xinan1019@gmail.com>
2024-12-12 11:20:50 +0800

    bmi finish

--- src/main/java/com/sean/Hello.kt
+++ src/main/java/com/sean/Hello.kt
@@ -6,14 +6,14 @@ fun main() {
     var human = Human(60.5f,1.8f);
     human.Hello();
     println(human.bmi())
-    val age : Int = 19; //val 名稱 : 型態 = 值
+    val age : Int = 19;      // 名稱 : 型態 = 值
     //val 不可改變 var 可變
 
     var weight = 66.5f;
     var name : String;
     name = "Sean";
 }
-class Human(var weight : Float,var height : Float){
+class Human( var weight : Float,var height : Float){
     fun bmi() : Float {
         var  bmi = weight / (height * height)
         return bmi;
