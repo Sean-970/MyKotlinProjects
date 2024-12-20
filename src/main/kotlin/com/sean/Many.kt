76a4c82129383b8444d78b0df17c49b3836d53d7
Sean <xinan1019@gmail.com>
2024-12-20 10:11:51 +0800

    mutableList finish

--- src/main/kotlin/com/sean/Many.kt
+++ src/main/kotlin/com/sean/Many.kt
@@ -1,9 +1,13 @@
 package com.sean
 
 fun main() {
-    val list = listOf(1,2,3,4)
+    val list = listOf(1,2,3,4)  //不可改
     println(list)
     for ( lists in  list){
         println(lists)
     }
+
+    var mutableList = mutableListOf(1,2,3,4,)  //可寫
+        mutableList.add(5)
+        println(mutableList)
 }
